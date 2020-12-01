c64ef13e6d98e9999ba04a3def7fa2483a2f7b6b
Ivan E. Cao-Berg <icaoberg@andrew.cmu.edu>
2020-11-30 21:11:47 -0500

    Updated repo for Bridges2

---
+++ 2.23.2/modulefile.lua
@@ -0,0 +1,51 @@
+--
+-- picard 2.23.2 modulefile
+--
+-- "URL: https://www.psc.edu/resources/software"
+-- "Category: Biological Sciences"
+-- "Description: Picard is a set of command line tools for manipulating high-throughput sequencing (HTS) data and formats such as SAM/BAM/CRAM and VCF."
+-- "Keywords: singularity bioinformatics"
+
+whatis("Name: picard")
+whatis("Version: 2.23.2")
+whatis("Category: Biological Sciences")
+whatis("URL: https://www.psc.edu/resources/software")
+whatis("Description: Picard is a set of command line tools for manipulating high-throughput sequencing (HTS) data and formats such as SAM/BAM/CRAM and VCF.")
+
+help([[
+picard 2.23.2
+-------------
+
+Description
+-----------
+Picard is a set of command line tools for manipulating high-throughput sequencing (HTS) data and formats such as SAM/BAM/CRAM and VCF.
+
+To load the module type
+
+> module load picard/2.23.2
+
+To unload the module type
+
+> module unload picard/2.23.2
+
+Documentation
+-------------
+https://github.com/pezmaster31/picard/wiki
+
+For help, type
+
+>
+
+Repository
+----------
+https://github.com/pezmaster31/picard
+
+Tools included in this module are
+
+* picard
+]])
+
+local package = "picard"
+local version = "2.23.2"
+local base    = pathJoin("/opt/packages",package,version)
+prepend_path("PATH", base)
--- 2.23.2/pull.sh
+++ 2.23.2/pull.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 singularity pull https://depot.galaxyproject.org/singularity/picard:2.23.2--0
-mv -v picard:2.23.2--0 singularity-picard-2.23.2
+mv -v picard:2.23.2--0 singularity-picard-2.23.2.sif
